173493d68736d3f9202ddf5855326cd8ffe03401
Jeff Walker <cavedog123@gmail.com>
2015-03-20 22:48:39 -0500

    Added empty b

---
+++ BootstrapSite1/BootstrapSite1/Controllers/PortfolioController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+
+namespace BootstrapSite1.Controllers
+{
+    
+    
+    public class PortfolioController : Controller
+    {
+        
+        public ActionResult OneColumn()
+        {
+            return View();
+        }
+    }
+}
